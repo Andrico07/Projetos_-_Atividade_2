93929369861b740ba0611041b65eb7b2c71d3632
Andrico <andricominarrini@gmail.com>
2022-07-13 00:03:11 -0300

    Update Principal.java

--- GestaoGraficos/src/main/java/com/mycompany/gestaograficos/Principal.java
+++ GestaoGraficos/src/main/java/com/mycompany/gestaograficos/Principal.java
@@ -1,9 +1,12 @@
-package com.mycompany.gestaograficos;
+ package com.mycompany.gestaograficos;
+
+import com.mycompany.presenter.MainPresenter;
+import java.io.IOException;
 
 public class Principal {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
-        //new MainPresenter();
+        new MainPresenter();
     }
 }
